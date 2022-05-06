a4ae12ffc2de36190b59e08ce999fe7d3201ad9e
= <=>
2022-05-05 22:08:41 -0500

    Mostrar usuarios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -3,19 +3,26 @@ import { reqResApi } from '../api/reqRes';
 import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
-        // use state de tipp  arrego de usuarios 
+    // use state de tipp  arrego de usuarios 
     const [usuarios, setUsuarios] = useState<Usuario[]>([])
-
-
 
     useEffect(() => {
         // lamado al API
         reqResApi.get<ReqResListado>('/users')
         .then(resp =>{
-            console.log(resp.data.data)
+            setUsuarios(resp.data.data)
         }).catch(console.log)
     }, []);
-   
+   // El  renderItem de ciclo dbe tener el key que debe ser tipo string
+    const renderItem = (usuario: Usuario) =>{
+        return (
+            <tr key={usuario.id.toString()}>
+                <td><img src={usuario.avatar} style={{ width:30, borderRadius:100}} alt={usuario.first_name}/></td>
+                <td>{usuario.first_name +' '+ usuario.last_name}</td>
+                <td>{usuario.email}</td>
+            </tr>
+        )
+    }
   
   
 return (
@@ -29,6 +36,14 @@ return (
                     <th>Email</th>
                 </tr>
             </thead>
+            <tbody>
+                {
+                    /* Forma larga 
+                    usuarios.map((usuario) => renderItem(usuario)) */
+                    usuarios.map(renderItem)
+                    
+                }
+            </tbody>
         </table>
     </>
   )
