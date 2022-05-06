93fcfb58a9596870085fbb558c516231d48b3900
= <=>
2022-05-05 22:55:15 -0500

    Paginado Simple

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { reqResApi } from '../api/reqRes';
 import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
@@ -6,13 +6,36 @@ export const Usuarios = () => {
     // use state de tipp  arrego de usuarios 
     const [usuarios, setUsuarios] = useState<Usuario[]>([])
 
+    // Cuando no se necesita renderizar en jsx se usa useRef
+    const pageRef = useRef(1);
+
+
     useEffect(() => {
         // lamado al API
-        reqResApi.get<ReqResListado>('/users')
-        .then(resp =>{
-            setUsuarios(resp.data.data)
-        }).catch(console.log)
+        cargarUsuarios();
     }, []);
+
+    const cargarUsuarios = async () =>{
+       const resp = await reqResApi.get<ReqResListado>('/users', {
+           params: {
+               //  .current referencia al valor de page
+               page:pageRef.current 
+           }
+       })
+       if(resp.data.data.length > 0){
+            setUsuarios(resp.data.data)
+            pageRef.current++;
+       }else{
+           alert('Sin registros');
+       }
+       console.log(pageRef.current)
+       
+    }
+
+   // Obtener usuarios
+
+
+
    // El  renderItem de ciclo dbe tener el key que debe ser tipo string
     const renderItem = (usuario: Usuario) =>{
         return (
@@ -45,6 +68,10 @@ return (
                 }
             </tbody>
         </table>
+        <button className='btn btn-primary'
+            onClick={cargarUsuarios}>
+            Siguiente
+        </button>
     </>
   )
 }
